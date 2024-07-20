8e911163de665c8abe9160b39750bcd88f8796da
kibrit74 <159079559+kibrit74@users.noreply.github.com>
2024-07-20 04:44:04 +0300

    app.py

--- app.py
+++ app.py
@@ -3984,10 +3984,11 @@ def logout():
 
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5001))
+    app.run(debug=True, host='0.0.0.0', port=port)if __name__ == "__main__":
 
 
-    app.run(debug=True)
-    
+
     
     
     
